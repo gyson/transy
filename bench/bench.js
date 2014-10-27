2211bbc55b4e63b29ecba3e3a629b81a579016f0
gyson <eilian.yunsong@gmail.com>
2014-10-27 15:00:10 +0800

    add bench with transducers.js

--- bench/bench.js
+++ bench/bench.js
@@ -1,16 +1,14 @@
 
-var fast = require('fast.js')
 var ty = require('../lib/index')
-var Benchmark = require('benchmark')
-
+//var Benchmark = require('benchmark')
 
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-for (var i = 0; i < 1000000; i++) {
+for (var i = 0; i < 10; i++) {
     arr.push(i);
 }
 
-var inc = function (x) { return x * x + Math.random() }
+var inc = function (x) { return x * x }
 
 var isEven = function (x) { return x % 2 === 0 }
 
@@ -22,54 +20,41 @@ var composed = ty.chain(function (chain) { chain
     .map(inc)
 })
 
-function max() {
-    var result = []
-    for (var i = 0, len = arr.length; i < len; i++) {
-        var x = arr[i];
-        x = inc(x)
-        x = inc(x)
-        if (!isEven(x)) { continue }
-        x = inc(x)
-        x = inc(x)
-        result.push(x)
-    }
-    return result
+exports['transy'] = function () {
+    return ty.array(composed, arr)
 }
 
-function fastjs() {
+var jt = require('transducers.js')
+var jtForm = jt.compose(
+    jt.map(inc),
+    jt.map(inc),
+    jt.filter(isEven),
+    jt.map(inc),
+    jt.map(inc)
+)
+
+exports['jlongster-transducer.js'] = function () {
+    return jt.into([], jtForm, arr)
+}
+
+var ct = require('transducers-js')
+var ctForm = ct.comp(
+    ct.map(inc),
+    ct.map(inc),
+    ct.filter(isEven),
+    ct.map(inc),
+    ct.map(inc)
+)
+exports['Cognitect-transducers-js'] = function () {
+    return ct.into([], ctForm, arr)
+}
+
+var fast = require('fast.js')
+
+exports['fast.js'] = function () {
     return fast.map(fast.map(fast.filter(fast.map(fast.map(arr, inc), inc), isEven), inc), inc)
 }
 
-// transy
-function transy() {
-    return ty.array(composed, arr)
-}
-//
-// console.log(transy())
-// console.log(array())
-// console.log(fastjs())
-
-
-function array() {
+exports['native'] = function () {
     return arr.map(inc).map(inc).filter(isEven).map(inc).map(inc)
 }
-
-new Benchmark.Suite()
-
-.add('transy', transy)
-
-.add('array', array)
-
-.add('fastjs', fastjs)
-
-.add('max', max)
-
-.on('cycle', function(event) {
-  console.log(String(event.target));
-})
-
-.on('complete', function() {
-  console.log('Fastest is ' + this.filter('fastest').pluck('name'));
-})
-
-.run({ 'async': true });
